348d5e59d2a4dd0fa4454dc2e6ab94ebc597e84e
abdullahalwafi1 <abdullahalwafi16@gmail.com>
2023-10-05 15:21:45 +0700

    upload pertemuan 2

---
+++ Pertemuan 2/Praktikum2.java
@@ -0,0 +1,18 @@
+public class Praktikum2 {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b  = 2;
+        float y = 3;
+
+        double c = Math.sqrt((a * a) + (b * b) - ((2 * a * b) * Math.cos(y)));
+        
+        System.out.println("Nama: Abdullah Al Wafi");
+        System.out.println("NIM: 0110222103");
+        System.out.println("Alamat: Bogor");
+
+        System.out.println("Hasil dari soal tersebut :" +c);
+    }
+
+}
